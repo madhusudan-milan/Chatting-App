95009032889a430c56ac9edd69d0ba9b41720262
Chirag <chirag657krsingh@gmail.com>
2023-06-02 19:38:44 +0530

    Splash Screen and getting Started pages made

---
+++ app/src/main/java/com/example/chattingapp/Login_Signup/Login_Screen.java
@@ -0,0 +1,16 @@
+package com.example.chattingapp.Login_Signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chattingapp.R;
+
+public class Login_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
