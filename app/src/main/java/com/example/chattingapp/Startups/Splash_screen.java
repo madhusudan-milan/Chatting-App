2e41f09a4b1fa258853c3412f4a755dcda6b0b34
ms933 <ms9338504@gmail.com>
2023-06-24 17:41:35 +0530

    new design added

--- app/src/main/java/com/example/chattingapp/Startups/Splash_screen.java
+++ app/src/main/java/com/example/chattingapp/Startups/Splash_screen.java
@@ -6,15 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.chattingapp.Login_Signup.Getting_started;
 import com.example.chattingapp.R;
 
 public class Splash_screen extends AppCompatActivity {
 
+    LottieAnimationView lottie;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        lottie = findViewById(R.id.lottie);
+        lottie.playAnimation();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -24,7 +30,7 @@ public class Splash_screen extends AppCompatActivity {
                 finish();
 
             }
-        },1500);
+        },2500);
 
 
     }
--- app/src/main/java/com/example/chattingapp/signup_login/Signup_page.java
+++ app/src/main/java/com/example/chattingapp/signup_login/Signup_page.java
@@ -6,16 +6,19 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.chattingapp.R;
+import com.example.chattingapp.dashboard.Home_main_screen;
 
 public class Signup_page extends AppCompatActivity {
 
     LottieAnimationView lottie;
 
-    TextView signup,hhhh;
+    TextView tv_signup,hhhh;
+    Button btn_signup;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -23,18 +26,22 @@ public class Signup_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
 
-        signup = findViewById(R.id.tv_alreadyhaveanaccount);
+        tv_signup = findViewById(R.id.tv_alreadyhaveanaccount);
+        btn_signup=findViewById(R.id.btn_signup);
 
         lottie = findViewById(R.id.lottie);
         lottie.playAnimation();
 
-        signup.setOnClickListener(view -> {
+        tv_signup.setOnClickListener(view -> {
             Intent i = new Intent(Signup_page.this,Login_page.class);
             i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
             startActivity(i);
             finish();
         });
-
+        btn_signup.setOnClickListener(v -> {
+            startActivity(new Intent(Signup_page.this, Home_main_screen.class));
+            finish();
+        });
 
     }
 }
